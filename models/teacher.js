7092f68b42265b22b552bb665c50539ccb29b545
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-04-24 23:03:04 +0800

    feat:create teacer model

---
+++ models/teacher.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Teacher extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Teacher.init({
+    email: DataTypes.STRING,
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    avatar: DataTypes.STRING,
+    total_favorite: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Teacher',
+  });
+  return Teacher;
+};
