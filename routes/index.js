c3994c222ab0c198b7482a375c092c0e39e3718a
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-04-26 21:45:09 +0800

    feat:add teacher signin feature

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ const express = require("express")
 const router = express.Router()
 const passport = require("../config/passport")
 
+// 老師登入
 router.post(
-  "/api/signin",
+  "/api/teacher/signin",
+  passport.authenticate("local", { session: false }),
+  teacherController.signIn
+)
+
+// 學生登入
+router.post(
+  "/api/user/signin",
   passport.authenticate("local", { session: false }),
   userController.signIn
 )
