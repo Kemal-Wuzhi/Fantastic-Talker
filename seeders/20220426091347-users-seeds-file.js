72c93e417834ddb994b410809445c69b921cc2c0
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-04-26 17:20:59 +0800

    add User seeder

---
+++ seeders/20220426091347-users-seeds-file.js
@@ -0,0 +1,29 @@
+// User 2 個學生（role: user, email: user1@example.com, password: 12345678…）
+"use strict"
+const bcrypt = require("bcryptjs")
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Users", [
+      {
+        email: "user1@example.com",
+        name: "user1",
+        password: await bcrypt.hash("12345678", 10),
+        role: "user",
+        updatedAt: new Date(),
+        createdAt: new Date(),
+      },
+      {
+        email: "user2@example.com",
+        name: "user2",
+        password: await bcrypt.hash("12345678", 10),
+        role: "user",
+        updatedAt: new Date(),
+        createdAt: new Date(),
+      },
+    ])
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null)
+  },
+}
