a8255663d41460fae31e2911c6d63a79e4fb2015
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-05-01 23:54:24 +0800

    feat:init getUser feature

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,5 +4,7 @@ const userController = require("../../controllers/user-controller")
 
 //學生註冊功能
 router.post("/", userController.signUp)
+//取得學生個人資料
+router.get("/:id", userController.getUser)
 
 module.exports = router
