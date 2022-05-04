cd98dd48b5ecae94b50b3a6cc3b7776b17eb51b4
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-05-04 17:54:33 +0800

    fix:fix merge conflicts

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express")
 const app = express()
+const passport = require("./config/passport")
+const routes = require("./routes")
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(passport.initialize())
+app.use(routes)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
