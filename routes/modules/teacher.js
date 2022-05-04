0a7faa10dcb8c7bab4b8e987bcbf84afb16f3ab3
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-05-04 23:11:22 +0800

    modify:delete getCurrentUser route

--- routes/modules/teacher.js
+++ routes/modules/teacher.js
@@ -8,7 +8,5 @@ router.post("/", teacherController.signUp)
 router.get("/:id", teacherController.getTeacher)
 //修改老師個人資料
 router.put("/:id", teacherController.putTeacher)
-//取得現在登入的老師
-router.get("/current_user", teacherController.getCurrentTeacher)
 
 module.exports = router
