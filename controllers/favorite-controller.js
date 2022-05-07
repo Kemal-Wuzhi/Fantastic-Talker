ad828ead173f78bdebde748f925222a00a2ffc76
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-05-07 18:14:28 +0800

    feat:init favoriteController feature

--- controllers/favorite-controller.js
+++ controllers/favorite-controller.js
@@ -1,8 +1,15 @@
 const { Favorite } = require("../models")
 
 const favoriteController = {
+  //postFavorites 只有學生對老師
   postFavorites: async (req, res, next) => {
     try {
+      const targetTeacherId = Number(req.body.id)
+      if (!targetTeacherId) throw new Error("此名老師不存在")
+      //判斷是否重複收藏
+      //建立收藏關係
+      const result = await Promise.all(Favorite.create({}))
+      //model 是否需要設立關連
     } catch (err) {
       next(err)
     }
