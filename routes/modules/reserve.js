9ec43f7e20223911e0e9d85dc3d49f66724865d6
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-05-14 22:44:59 +0800

    modify:modify post reserves route

--- routes/modules/reserve.js
+++ routes/modules/reserve.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const reserveController = require("../../controllers/reserve-controller")
 const { authenticatedUser } = require("../../middleware/auth")
 
-router.post("/:teacherId", authenticatedUser, reserveController.postReserves)
+router.post("/", authenticatedUser, reserveController.postReserves)
 router.delete("/:id", authenticatedUser, reserveController.deleteReserves)
 
 module.exports = router
